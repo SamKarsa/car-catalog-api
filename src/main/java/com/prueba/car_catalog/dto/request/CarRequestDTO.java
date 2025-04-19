31a3ba4eeff4d6b8a8e6e5b4aedbba64d241ced3
SamKarsa <samuellopezmarin2006@gmail.com>
2025-04-19 14:04:46 -0500

    feat: add validations to CarRequestDTO
    Terminé de poner las validaciones necesarias en el DTO CarRequestDTO.

--- src/main/java/com/prueba/car_catalog/dto/request/CarRequestDTO.java
+++ src/main/java/com/prueba/car_catalog/dto/request/CarRequestDTO.java
@@ -7,10 +7,11 @@ import lombok.Data;
 public class CarRequestDTO {
     @NotBlank(message = "Model is required")
     @Size(max = 30, message = "Model must be less than 30 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9 \\-]+$", message = "Model can only contain letters, numbers, spaces and hyphens")
     private String model;
 
     @NotBlank(message = "Description is required")
-    @Size(max = 100, message = "Model must be less than 100 characters")
+    @Size(min = 10, max = 100, message = "Model must be between 10 and 100 characters")
     private String description;
 
     @NotNull(message = "Price is required")
@@ -22,5 +23,6 @@ public class CarRequestDTO {
     private Integer mileage;
 
     @NotNull(message = "Brand ID is required")
+    @Positive(message = "Id must be a positive number")
     private Integer brandId;
 }
